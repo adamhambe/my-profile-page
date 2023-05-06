4bcbbdea953565e1188f9b7aef12fd4fe13d0a9e
adam.hambe@stralfors.se <adam.hambe@stralfors.se>
2023-05-06 11:04:14 +0200

    Initial commit

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -8,22 +8,3 @@ export default defineComponent({
     components: { ExpenseTracker }
 });
 </script>
-
-<template>
-	<div class="standard-page-layout" id="profile">
-		<div class="standard-page">
-			<ul class="flex-container">
-				<li class="flex-item">
-					<router-link to="/fitness-tracker">Fitness Tracker</router-link>
-				</li>
-				<li class="flex-item">
-					<router-link to="/habit-tracker">Habit Tracker</router-link>
-				</li>
-				<li class="flex-item">
-					<router-link to="/expense-tracker">Expense Tracker</router-link>
-				</li>
-			</ul>
-		</div>
-	</div>
-</template>
-<style scoped src="./style.scss" lang="scss"></style>
