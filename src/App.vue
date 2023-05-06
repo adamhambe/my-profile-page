9805fdce9ca12a73a1dcc6751448fdb81678914e
Adam Hambe <adamhambe@gmail.com>
2023-05-06 09:11:36 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from './pages/Header/Header.vue';
+// import Profile from './pages/Profile/Profile.vue';
+// import Navbar from './components/Navbar/Navbar.vue';
+
+</script>
+
+<template>
+  <Header></Header>
+</template>
+
+<style lang="scss">
+@import './styles/page-layouts.scss';
+@import './styles/style.scss';
+</style>
---
+++ src/pages/Profile/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="standard-page-layout" id="profile">
+		<NavbarComp />
+		<div class="standard-page">
+			<ul class="grid-container">
+				<li class="grid-item">
+					<router-link to="/task-manager">Task Manager</router-link>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" >
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+
+export default defineComponent({
+
+
+});
+</script>
+<style scoped src="./style.scss" lang="scss"></style>
