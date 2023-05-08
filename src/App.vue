f4609357ff2ad622bb60e0c16d2d391842f7cb49
adam.hambe@stralfors.se <adam.hambe@stralfors.se>
2023-05-08 20:23:59 +0200

    added vue-router and a few links

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import Header from './pages/Header/Header.vue';
-import Profile from './pages/Profile/Profile.vue';
-// import Profile from './pages/Profile/Profile.vue';
+import Home from './views/Home/Home.vue';
+import Profile from './views/Profile/Profile.vue';
+// import Profile from './views/Profile/Profile.vue';
 // import Navbar from './components/Navbar/Navbar.vue';
 
 </script>
 
 <template>
-  <Header></Header>
+  <Home></Home>
   <Profile/>
 </template>
 
